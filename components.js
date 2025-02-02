d07b812bb84b58067313faa9d31e3869e7137943
Banaanae <83927639+Banaanae@users.noreply.github.com>
2025-02-02 12:38:56 +1100

    food and consumable
    (consumable is wip)

--- components.js
+++ components.js
@@ -1,4 +1,122 @@
-const universal = ['custom_name', 'damage', 'glider', 'item_model', 'jukebox_playable', 'lore', 'max_damage', 'max_stack_size', 'rarity', 'unbreakable'];
+const universal = ['custom_name', 'damage', 'food', 'glider', 'item_model', 'jukebox_playable', 'lore', 'max_damage', 'max_stack_size', 'rarity', 'unbreakable'];
+
+function build_consumable(components) {
+    let span = addComponent('consumable', 'number')
+
+    span.children[2].step = 0.1
+
+    span.appendChild(buildSelect(['none', 'eat', 'drink', 'block', 'bow', 'spear', 'crossbow', 'spyglass', 'toot_horn', 'brush']))
+
+    let sound = document.createElement('input')
+    sound.type = 'text'
+    span.appendChild(sound)
+
+    let has_consume_particles = document.createElement('input')
+    has_consume_particles.type = 'checkbox'
+    span.appendChild(has_consume_particles)
+
+    let on_consume_effects = buildSelect(['apply_effects', 'remove_effects', 'clear_all_effects', 'teleport_randomly', 'play_sound'], true)
+    span.appendChild(on_consume_effects)
+
+    let apply_effects = document.createElement('span')
+    apply_effects.style.display = 'none'
+    span.appendChild(apply_effects)
+
+    let remove_effects = document.createElement('span')
+    remove_effects.style.display = 'none'
+    let wrapper = document.createElement('span')
+    remove_effects.appendChild(wrapper)
+
+    let firstEffect = document.createElement('input')
+    firstEffect.type = 'text'
+    wrapper.appendChild(firstEffect)
+
+    let pBtn = document.createElement('button')
+    pBtn.innerText = '+'
+    pBtn.addEventListener('click', function () {
+        let input = document.createElement('input')
+        input.text = 'text'
+        wrapper.appendChild(input)
+    })
+
+    let mBtn = document.createElement('button')
+    mBtn.innerText = '-'
+    mBtn.addEventListener('click', function () {
+        document.querySelector('#consumable > span:nth-child(9) > span > input:last-child').remove()
+    })
+
+    remove_effects.appendChild(wrapper)
+    remove_effects.appendChild(pBtn)
+    remove_effects.appendChild(mBtn)
+    span.appendChild(remove_effects)
+
+    let clear_all_effects = document.createElement('span')
+    clear_all_effects.style.display = 'none'
+    span.appendChild(clear_all_effects)
+
+    let teleport_randomly = document.createElement('span')
+    teleport_randomly.style.display = 'none'
+    let diameter = document.createElement('input')
+    diameter.type = 'number'
+    diameter.step = '0.1'
+    teleport_randomly.appendChild(diameter)
+    span.appendChild(teleport_randomly)
+
+    let play_sound = document.createElement('span')
+    play_sound.style.display = 'none'
+    let sound2 = document.createElement('input')
+    sound2.type = 'text'
+    play_sound.appendChild(sound2)
+    span.appendChild(play_sound)
+
+
+    on_consume_effects.addEventListener('change', function () {
+        for (let i = 0; i < on_consume_effects.length; i++) {
+            document.querySelector('#consumable > span:nth-child(' + (i + 8) + ')').style.display = (on_consume_effects[i].selected) ? 'inline' : 'none'
+        }
+    })
+
+    components.appendChild(span)
+}
+
+function consumable(arr) {
+    let component = 'consumable={'
+
+    if (arr[0] !== 1.6)
+        component += `consume_seconds:${arr[0]},`
+
+    if (arr[1] !== 'eat')
+        component += `animation:"${arr[1]}",`
+
+    if (arr[2] !== 'entity.generic.eat')
+        component += `sound:"${arr[2]}",`
+
+    if (arr[3] !== 'true')
+        component += 'has_consume_particles:false,'
+
+    let consumeFX = 'on_consume_effects:['
+
+    if (arr[4].apply_effects) {
+        // TODO
+    }
+
+    if (arr[4].remove_effects) {
+        // TODO
+    }
+
+    if (arr[4].clear_all_effects)
+        consumeFX += '{type:"minecraft:clear_all_effects"},'
+
+    if (arr[4].teleport_randomly)
+        consumeFX += `{type:"minecraft:teleport_randomly",diameter:${arr[arr.length - 2]}},`
+
+    if (arr[4].play_sound)
+        consumeFX += `{type:"minecraft:play_sound",sound:"${arr[arr.length - 1]}"},`
+
+    if (consumeFX !== 'on_consume_effects:[')
+        return component + consumeFX.replace(/,$/, ']}]')
+    return component.replace(/,$/, '}]')
+}
 
 function build_custom_name(components) {
     let span = addComponent('custom_name', 'text')
@@ -36,6 +154,29 @@ function build_damage(components) {
 
 function damage(arr) {
     return `damage=${arr[0]}`
+}
+
+function build_food(components) {
+    let span = addComponent('food', 'number')
+
+    let saturation = document.createElement('input')
+    saturation.type = 'number'
+    saturation.step = '0.1'
+    span.appendChild(saturation)
+
+    let can_always_eat = document.createElement('input')
+    can_always_eat.type = 'checkbox'
+    span.appendChild(can_always_eat)
+
+    components.appendChild(span)
+}
+
+function food(arr) {
+    let component = `food={nutrition:${arr[0]},saturation:${arr[1]}`
+
+    if (arr[2] !== 'false')
+        component += ',can_always_eat:true'
+    return component + '}'
 }
 
 function build_glider(components) {
